e80990a995f03b42d8263ef8fd45101bba8fccf7
Gaëtan H <gaetanhus@gmail.com>
2024-10-08 19:22:55 +0200

    style(page.tsx): improve code readability by restructuring JSX elements for better organization and clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import { Customizer } from "@/components/customizer";
+import Link from "next/link";
 
 const Page = () => {
   return (
-    <div className="flex items-center justify-center p-3 sm:mt-[10vh]">
-      <Customizer />
-    </div>
+    <>
+      <div className="flex items-center justify-center p-3 sm:mt-[10vh]">
+        <Customizer />
+      </div>
+    
+      <div className="flex items-center justify-center p-3">
+        <p className="text-center text-sm text-gray-500">
+          This is a <Link className="text-blue-300 hover:underline" href="https://github.com/Steellgold/qrcode">open-source</Link> project. <br />Feel free to contribute! 🚀
+        </p>
+      </div>
+    </>
   );
 }
 
